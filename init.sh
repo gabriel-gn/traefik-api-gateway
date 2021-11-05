deafb4188f2c827d555d9cff136a61eede821308
Gabriel Nogueira <gabrielgomesnogueira@gmail.com>
2021-11-05 14:32:16 -0300

    :sparkles: mock services

--- init.sh
+++ init.sh
@@ -6,4 +6,5 @@ docker network create kong-net # --scope=swarm
 
 # docker stack deploy -c docker-compose.yml traefik
 docker-compose -f docker-compose.yml -p traefik up --remove-orphans -d
+docker-compose -f services-compose.yml -p mock-services up --remove-orphans -d
 docker-compose -f kong-compose.yml -p api-gateway up --remove-orphans -d
